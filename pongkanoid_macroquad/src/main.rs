4671dbec3bab17af57d5103e2f2dad535bc7e9cd
tlietz <theodore.lietz@gmail.com>
2021-12-24 15:16:51 -0600

    adds ball and moveable platform

--- pongkanoid_macroquad/src/main.rs
+++ pongkanoid_macroquad/src/main.rs
@@ -2,14 +2,46 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Pongkanoid")]
 async fn main() {
+    const SCREEN_W: f32 = 20.0;
+    const SCREEN_H: f32 = 20.0;
+
+    let mut platform_x = SCREEN_W / 2.;
+    let platform_width = 2.;
+    let platform_height = 0.5;
+
+    // initialize the ball to be sitting on the top of the platform
+    let mut ball_radius = platform_height * 0.5;
+    let mut ball_x = platform_x;
+    let mut ball_y = SCREEN_H - (ball_radius + platform_height);
+
+    // builds camera with following coordinate system:
+    // (0, 0)        ... (SCREEN_W, 0.)
+    // (0, SCREEN_H) ... (SCREEN_W, SCREEN_H)
+    set_camera(&Camera2D {
+        zoom: vec2(1. / SCREEN_W * 2., -1. / SCREEN_H * 2.),
+        target: vec2(SCREEN_W / 2., SCREEN_H / 2.),
+        ..Default::default()
+    });
+
     loop {
-        clear_background(RED);
+        let delta = get_frame_time();
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        if is_key_down(KeyCode::Right) && platform_x + platform_width / 2. < SCREEN_W {
+            platform_x += 5. * delta;
+        }
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        if is_key_down(KeyCode::Left) && platform_x - platform_width / 2. > 0. {
+            platform_x -= 5. * delta;
+        }
+
+        draw_circle(ball_x, ball_y, ball_radius, RED);
+        draw_rectangle(
+            platform_x - platform_width / 2.,
+            SCREEN_H - platform_height,
+            platform_width,
+            platform_height,
+            WHITE,
+        );
 
         next_frame().await
     }
