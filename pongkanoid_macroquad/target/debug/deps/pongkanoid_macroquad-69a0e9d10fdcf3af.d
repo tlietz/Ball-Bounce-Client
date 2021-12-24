93d13f09ef53c86096a410bd248ace0854b56871
tlietz <theodore.lietz@gmail.com>
2021-12-24 10:26:26 -0600

    update .gitignore

--- pongkanoid_macroquad/target/debug/deps/pongkanoid_macroquad-69a0e9d10fdcf3af.d
+++
@@ -1,5 +0,0 @@
-/Users/tlietz/Developer/Games/Pongkanoid/pongkanoid_macroquad/target/debug/deps/pongkanoid_macroquad-69a0e9d10fdcf3af: src/main.rs
-
-/Users/tlietz/Developer/Games/Pongkanoid/pongkanoid_macroquad/target/debug/deps/pongkanoid_macroquad-69a0e9d10fdcf3af.d: src/main.rs
-
-src/main.rs:
--- pongkanoid_macroquad/target/debug/pongkanoid_macroquad.d
+++
@@ -1,1 +0,0 @@
-/Users/tlietz/Developer/Games/Pongkanoid/pongkanoid_macroquad/target/debug/pongkanoid_macroquad: /Users/tlietz/Developer/Games/Pongkanoid/pongkanoid_macroquad/src/main.rs
